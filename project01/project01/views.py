dd2369c76f33222e463c7cd0eb146e60e651293d
Cheong Myeong Noh <cjdauddl93@gmail.com>
2021-07-26 14:20:43 +0900

    Team1_day10_finish_02

--- project01/project01/views.py
+++ project01/project01/views.py
@@ -5,7 +5,6 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from myanalysis.openapi import OpenAPI
 from .models import User
 from matplotlib import pyplot as plt
 from plotly.offline import plot
@@ -338,6 +337,6 @@ def summary2(request):
 
 	return render(request, 'summary2.html')
 
-def highchart(request):
-    data = OpenAPI().high();
-    return HttpResponse(json.dumps(data), content_type='application/json');
+# def highchart(request):
+#     data = OpenAPI().high();
+#     return HttpResponse(json.dumps(data), content_type='application/json');
